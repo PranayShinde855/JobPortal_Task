42ab3b181d3cf48bfbe39bba497cb80e32fa65f2
pranayshinde855 <pranayshinde855@gmail.com>
2022-04-06 10:21:44 +0530

    minor changes

--- API/Startup.cs
+++ API/Startup.cs
@@ -172,23 +172,6 @@ namespace API
             {
                 endpoints.MapControllers();
             });
-
-            //app.UseExceptionHandler(
-            //     options =>
-            //    {
-            //        options.Run(async context =>
-            //        {
-            //            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-            //            context.Response.ContentType = "text/html";
-            //            var exceptionObject = context.Features.Get<IExceptionHandlerFeature>();
-            //            if (null != exceptionObject)
-            //            {
-            //                var errorMessage = $"{exceptionObject.Error.Message}";
-            //                await context.Response.WriteAsync(errorMessage).ConfigureAwait(false);
-            //            }
-            //        });
-            //    });
-
         }
     }
 }
